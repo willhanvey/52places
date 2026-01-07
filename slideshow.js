db8adb32c78146efa258a769304827dcd58eb2d9
William Hanvey <hanvey.w@northeastern.edu>
2026-01-06 23:46:35 -0500

    Home page pretty close to done

---
+++ slideshow.js
@@ -0,0 +1,41 @@
+const images = [
+    'images/hormuz.jpg',
+    'images/pamirhighway.jpg',
+    'images/turkistan.jpg'
+  ];
+  
+  let current = 0;
+  let slide1 = document.querySelector('.slide1');
+  let slide2 = document.querySelector('.slide2');
+  
+  // Initialize
+  slide1.style.backgroundImage = `url('${images[current]}')`;
+  slide1.style.opacity = 1;
+  slide2.style.opacity = 0;
+  
+  function crossfade() {
+    // Determine next image index
+    const next = (current + 1) % images.length;
+  
+    // fade in slide2
+    slide2.style.backgroundImage = `url('${images[next]}')`;
+    slide2.style.opacity = 1;
+  
+    // fade out slide1
+    slide1.style.opacity = 0;
+  
+    // after fade, swap roles
+    setTimeout(() => {
+      // swap variables
+      const temp = slide1;
+      slide1 = slide2;
+      slide2 = temp;
+  
+      // update current index
+      current = next;
+    }, 2000); // match transition duration
+  }
+  
+  // loop every 5 seconds
+  setInterval(crossfade, 5000);
+  
