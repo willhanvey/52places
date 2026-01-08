3bdf879528d3d3a14a05dc7b3f8140d150221a23
William Hanvey <hanvey.w@northeastern.edu>
2026-01-07 21:09:17 -0500

    Fixed sidebar bug

--- places.js
+++ places.js
@@ -1,20 +1,26 @@
 const places = [
     {
-      name: "Hormuz",
+      name: "Turkistan, Kazakhstan",
+      coords: [43.3, 68.3],
+      author: "BD82",
+      description: "Nestled a few hours across the Kazakh border from Tashkent on the ancient silk road, Turkistan is easily accessible by road, sleeper night train from Almaty, or via the spectacular Hazrat Sultan International Airport on flights from Kazakhstan and Istanbul. Declared \"Spiritual Capital of the Turkic World\" by the Organization of Turkic States, the city has started to draw wider attention from tourists as historic markets bleed right into Keruen-Saray, Central Asia's largest tourist complex. Here, modern Arabic-inspired architecture meets classical Kazakh and Uzbek influences, creating a cityscape unlike any other in the region. At the heart of the city fortress stands the UNESCO-listed Mausoleum of Khoja Ahmed Yasawi, a monumental complex whose beauty rivals even the famed architecture of Samarkand and Bukhara. For travelers looking to get off the Central Asian tourist trail, or to make an easy first stop in Kazakhstan for a night or two, Turkistan offers a blend of history and reinvention.<br><br>-BD82",
+      image: "images/turkistan.jpg"
+    },
+  
+    {
+      name: "Hormuz, Iran",
       coords: [27.06, 56.46],
+      author: "Bolthy",
       description: "Hormuz is a beautiful island in the Persian Gulf...",
       image: "images/hormuz.jpg"
     },
     {
-      name: "Pamir Highway",
+      name: "Pamir Highway, Tajikistan",
       coords: [38.5, 72.2],
+      author: "Rikk",
       description: "The Pamir Highway is a legendary road through Central Asia...",
       image: "images/pamirhighway.jpg"
-    },
-    {
-      name: "Turkistan",
-      coords: [43.3, 68.3],
-      description: "Turkistan is an ancient city in Kazakhstan...",
-      image: "images/turkistan.jpg"
     }
+
+    
   ];
